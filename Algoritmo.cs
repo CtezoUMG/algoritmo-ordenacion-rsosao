ddcf8457ec0b837d88010fee4399b1226eb10e8b
Roger Sosa <rogerthedeveloper@gmail.com>
2026-02-28 08:14:28 -0600

    Update Algoritmo.cs

--- Algoritmo.cs
+++ Algoritmo.cs
@@ -25,6 +25,17 @@ public class Algoritmo
     public void BubbleSort(int[] arr)
     {
         // TODO: Implementar el algoritmo de Bubble Sort tradicional
+        int n = arr.Length;
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n-i-1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {   
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                }
+            }
+        }
 
     }
 }
